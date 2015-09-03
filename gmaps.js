42b57ecc88afaea91c7b93f16221e1987789bd57
Kendansa <mrwein02@louisville.edu>
2015-09-02 21:41:03 -0400

    trying to get the gmap to work... aint' working.

--- gmaps.js
+++ gmaps.js
@@ -1,5 +1,27 @@
 new GMaps({
-  div: '#map',
+  div: '#googlemaps',
+  lat: 38.122432,
+  lng: -85.4223895,
+  width: 100%,
+  height: 100%,
+});
+ 
+map = new GMaps({
+  div: '#googlemaps',
+  zoom: 12,
   lat: 38.122432,
   lng: -85.4223895
+  }
+});
+
+map.addMarker({
+  lat: -12.043333,
+  lng: -77.028333,
+  title: 'Lima',
+  click: function(e) {
+    alert('You clicked in this marker');
+  }
+  infoWindow: {
+  content: '<p>HTML Content</p>'
+}
 });
